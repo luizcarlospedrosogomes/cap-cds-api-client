477609eb356abd2879bea9c7a7a841842c97925c
Luiz Carlos Pedroso Gomes <lc.pg@hotmail.com>
2026-01-30 18:39:25 +0000

    fix: build ts

--- srv/src/modules/erp/sap-services.ts
+++ srv/src/modules/erp/sap-services.ts
@@ -3,13 +3,14 @@ import { Service, Request } from "@sap/cds";
 
 
 module.exports = (services: Service) => {
-    services.on("checkService", async (req: any) => {
+    services.on("checkService", async (req: Request) => {
         const { data } = req
         const { serviceName, top } = data
         try {
             const middlewareApi = await cds.connect.to('middleware-api');
             const tx = middlewareApi.tx(req.query)
-            return await tx.send({path: `${serviceName}?$top=${top}`, method: 'GET'});
+            const results = await  tx.send({path: `${serviceName}?$top=${top}`, method: 'GET'});
+            return results;
         } catch (error: any) {
             console.error(error);
             return req.reject(error.response.status, error.message);
